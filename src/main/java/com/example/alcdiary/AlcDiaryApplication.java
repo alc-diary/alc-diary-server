7bda46810124a439439d863c81ac25abedc8fe44
dev-taeyeong <dev.taeyeong@gmail.com>
2022-09-03 11:26:42 +0900

    first commit

---
+++ src/main/java/com/example/alcdiary/AlcDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.alcdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlcDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlcDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/alcdiary/AlcDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alcdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlcDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
