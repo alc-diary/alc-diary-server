a3191986a69f7a069cc503d69a2f9f0400856723
dev-taeyeong <dev.taeyeong@gmail.com>
2022-10-09 01:37:42 +0900

    deploy test

--- src/main/java/com/example/alcdiary/presentation/HelloController.java
+++ src/main/java/com/example/alcdiary/presentation/HelloController.java
@@ -18,6 +18,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public ApiResponseModel<HelloResponse> hello() {
+
         return new ApiResponseModel<>(true, "message", helloUseCase.getHello());
     }
 }
