4cdbf5f56470b9d42029ee810a588f5aab5b0421
dev-taeyeong <dev.taeyeong@gmail.com>
2022-10-09 19:07:33 +0900

    modify: script 수정

--- scripts/health.sh
+++ scripts/health.sh
@@ -19,21 +19,21 @@ do
 
   if [ ${UP_COUNT} -ge 1 ]
   then
-    echo "> Health check 성공"
+    echo "> Health check success."
     switch_proxy
     break
   else
-    echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
+    echo "> The response of Health check is unknown or not in the running state."
     echo "> Health check: ${RESPONSE}"
   fi
 
   if [ ${RETRY_COUNT} -eq 10 ]
   then
-    echo "> Health check 실패. "
-    echo "> 엔진엑스에 연결하지 않고 배포를 종료합니다."
+    echo "> Health check fail."
+    echo "> Terminates deployment without connecting to nginx"
     exit 1
   fi
 
-  echo "> Health check 연결 실패. 재시도..."
+  echo "> Connection to Health check fail. retry..."
   sleep 10
 done
--- scripts/switch.sh
+++ scripts/switch.sh
@@ -7,11 +7,10 @@ source ${ABSDIR}/profile.sh
 function switch_proxy() {
   IDLE_PORT=$(find_idle_port)
 
-  echo "> 전환할 Port: $IDLE_PORT"
-  echo "> Port 전환"
+  echo "> Change port to $IDLE_PORT"
   echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" |
-  sudo tee /etc/nginx/conf.d/service-url.inc
+  sudo tee /etc/nginx/default.d/service-url.inc
 
-  echo "> 엔진엑스 Reload"
+  echo "> Nginx Reload"
   sudo service nginx reload
 }
