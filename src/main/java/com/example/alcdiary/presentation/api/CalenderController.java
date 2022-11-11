9b96f413cc3f06f6ec176f51199a4fd407830fc1
tape22 <gjwjdlas@gmail.com>
2022-11-11 22:13:58 +0900

    feat: 캘린더 생성 controller, request 추가

--- src/main/java/com/example/alcdiary/presentation/api/CalenderController.java
+++ src/main/java/com/example/alcdiary/presentation/api/CalenderController.java
@@ -1,22 +1,36 @@
 package com.example.alcdiary.presentation.api;
 
-import com.example.alcdiary.application.FindCalenderUseCase;
+import com.example.alcdiary.application.CalenderUseCase;
+import com.example.alcdiary.application.command.CreateCalenderCommand;
+import com.example.alcdiary.presentation.dto.request.CreateCalenderRequest;
 import com.example.alcdiary.presentation.dto.response.GetCalenderResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class CalenderController {
 
-    private final FindCalenderUseCase findCalenderUseCase;
+    private final CalenderUseCase calenderUseCase;
 
     @GetMapping(value = "/calender/{calender_id}")
     public GetCalenderResponse find(
             @PathVariable(name = "calender_id") Long calenderId
     ) {
-        return findCalenderUseCase.execute(calenderId).toResponse();
+        return calenderUseCase.find(calenderId).toResponse();
+    }
+
+
+    @PostMapping(value = "/calender/{user_id}")
+    public void create(
+            @RequestBody CreateCalenderRequest request
+    ) {
+        calenderUseCase.create(
+                CreateCalenderCommand.builder()
+                        .title(request.getTitle())
+                        .contents(request.getContents())
+                        .drinkType(request.getDrinkType())
+                        .hangOver(request.getHangOver()).build()
+        );
     }
 }
