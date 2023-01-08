d2961f2ef75a0367e84c9ecabba731e314900806
tape22 <gjwjdlas@gmail.com>
2023-01-08 12:56:10 +0900

    refactor: UserCalender 저장 시 enum 타입으로 저장되도록 수정

--- src/main/java/com/example/alcdiary/infrastructure/entity/UserCalender.java
+++ src/main/java/com/example/alcdiary/infrastructure/entity/UserCalender.java
@@ -6,10 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Builder
@@ -25,6 +22,7 @@ public class UserCalender {
     private Long calenderId;
 
     @Column(name = "edit_role")
+    @Enumerated(EnumType.STRING)
     private EditRole editRole;
 
 
