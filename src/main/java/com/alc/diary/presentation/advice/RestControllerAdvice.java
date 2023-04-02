24e48cb118f6f2547e39bf48654236a2f295c770
dev-taeyeong <dev.taeyeong@gmail.com>
2023-04-02 16:48:33 +0900

    controller advice에 로그 추가

--- src/main/java/com/alc/diary/presentation/advice/RestControllerAdvice.java
+++ src/main/java/com/alc/diary/presentation/advice/RestControllerAdvice.java
@@ -1,10 +1,12 @@
 package com.alc.diary.presentation.advice;
 
 import com.alc.diary.domain.exception.DomainException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @org.springframework.web.bind.annotation.RestControllerAdvice
 public class RestControllerAdvice {
 
@@ -17,6 +19,7 @@ public class RestControllerAdvice {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> exceptionHandler(Exception e) {
+        log.error("Error message: {}", e.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorResponse.getDefault());
     }
 }
