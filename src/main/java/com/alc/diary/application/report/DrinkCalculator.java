66b7fa8264dc020c599d3c5df53a40ebb245ac95
tape22 <gjwjdlas@gmail.com>
2023-04-02 14:05:09 +0900

    feat: report 추가

---
+++ src/main/java/com/alc/diary/application/report/DrinkCalculator.java
@@ -0,0 +1,4 @@
+package com.alc.diary.application.report;
+
+class DrinkCalculator {
+}
---
+++ src/test/java/com/alc/diary/application/user/SignInUseCaseTest.java
@@ -0,0 +1,10 @@
+package com.alc.diary.application.user;
+
+import org.junit.jupiter.api.Test;
+
+class SignInUseCaseTest {
+
+    @Test
+    void execute() {
+    }
+}
