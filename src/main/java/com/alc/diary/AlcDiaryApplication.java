23a62c899c218c7edee7fa5a5add6be1df9ed909
tape22 <gjwjdlas@gmail.com>
2023-04-02 13:02:12 +0900

    feat: application class 추가

---
+++ src/main/java/com/alc/diary/AlcDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.alc.diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlcDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlcDiaryApplication.class, args);
+    }
+}
+
