1612f579de94dfd714f80b3e18bbcb519704befb
dev-taeyeong <dev.taeyeong@gmail.com>
2023-04-02 13:31:07 +0900

    패키지 구조 변경

--- src/main/java/com/alc/diary/infrastructure/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.alc.diary.infrastructure;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository {
-}
---
+++ src/main/java/com/alc/diary/infrastructure/persistence/UserRepositoryImpl.java
@@ -0,0 +1,4 @@
+package com.alc.diary.infrastructure.persistence;
+
+public class UserRepositoryImpl {
+}
