a6d4a37bbf3244f74e8f493e05f4a51b0497e901
tape22 <gjwjdlas@gmail.com>
2023-04-02 14:05:09 +0900

    feat: report 추가

---
+++ src/main/java/com/alc/diary/application/report/ReportTmiUseCase.java
@@ -0,0 +1,6 @@
+package com.alc.diary.application.report;
+
+public class ReportTmiUseCase {
+    // 여기서 조회하되
+    // 데이터는 claulcator 에서 계산해서 응답값을 받아서 쓰고싶다.
+}
---
+++ src/main/java/com/alc/diary/domain/calender/service/CalenderService.java
@@ -0,0 +1,5 @@
+package com.alc.diary.domain.calender.service;
+
+public class CalenderService {
+    // db 데이터 핸들링.
+}
