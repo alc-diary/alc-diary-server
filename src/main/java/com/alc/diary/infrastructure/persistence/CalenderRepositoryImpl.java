4f87ffc177e3fd8568b4ac8f33744992ffac7315
dev-taeyeong <dev.taeyeong@gmail.com>
2023-04-02 15:50:13 +0900

    QueryDSL config 추가

--- src/main/java/com/alc/diary/infrastructure/persistence/CalenderRepositoryImpl.java
+++ src/main/java/com/alc/diary/infrastructure/persistence/CalenderRepositoryImpl.java
@@ -1,6 +1,12 @@
 package com.alc.diary.infrastructure.persistence;
 
 import com.alc.diary.domain.calender.repository.CustomCalenderRepository;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
+@RequiredArgsConstructor
+@Repository
 public class CalenderRepositoryImpl implements CustomCalenderRepository {
+    private final JPAQueryFactory jpaQueryFactory;
 }
--- src/main/java/com/alc/diary/infrastructure/persistence/UserRepositoryImpl.java
+++ src/main/java/com/alc/diary/infrastructure/persistence/UserRepositoryImpl.java
@@ -1,6 +1,12 @@
 package com.alc.diary.infrastructure.persistence;
 
 import com.alc.diary.domain.user.repository.CustomUserRepository;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
+@RequiredArgsConstructor
+@Repository
 public class UserRepositoryImpl implements CustomUserRepository {
+    private final JPAQueryFactory jpaQueryFactory;
 }
