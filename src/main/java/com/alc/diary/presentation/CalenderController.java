c46940be102b14b598dc9066d09cf80dce9a2021
tape22 <gjwjdlas@gmail.com>
2023-04-02 16:02:26 +0900

    feat: drinkType enum 추가 및 패키지 이동

--- src/main/java/com/alc/diary/presentation/CalenderController.java
+++ src/main/java/com/alc/diary/presentation/CalenderController.java
@@ -1,4 +1,25 @@
 package com.alc.diary.presentation;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/calender")
 public class CalenderController {
+    @PostMapping(value = "")
+    public void save() {
+
+    }
+
+    @PutMapping(value = "/{calenderId}")
+    // 권한 체크
+    public void update(@PathVariable Long calenderId) {
+
+    }
+
+    @DeleteMapping(value = "/{calenderId}")
+    public void delete(@PathVariable Long calenderId) {
+
+    }
 }
