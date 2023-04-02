ce04d9ff1c69ae0838bdfbbeded8f49a069ca34f
dev-taeyeong <dev.taeyeong@gmail.com>
2023-04-02 16:01:23 +0900

    Error model 수정

--- src/main/java/com/alc/diary/domain/calender/error/CalenderError.java
+++ src/main/java/com/alc/diary/domain/calender/error/CalenderError.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum CalenderError implements ErrorModel {
-    NO_ENTITY_FOUND("E0100", "No entity found");
+    NO_ENTITY_FOUND("E0100", "No entity found"),
+    INVALID_PARAMETER_INCLUDE("E0101", "Invalid parameter include");
 
     private final String code;
     private final String message;
