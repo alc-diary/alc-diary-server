a7b0ec721bb49180a7bbc1e82ea495bd4b496c27
dev-taeyeong <dev.taeyeong@gmail.com>
2023-04-02 16:08:39 +0900

    rest controller advice 수정

--- src/main/java/com/alc/diary/presentation/advice/ErrorResponse.java
+++ src/main/java/com/alc/diary/presentation/advice/ErrorResponse.java
@@ -1,5 +1,17 @@
 package com.alc.diary.presentation.advice;
 
-public class ErrorResponse {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private String code;
+    private String message;
+
+    public static ErrorResponse getDefault() {
+        return new ErrorResponse("E9999", "Internal server error");
+    }
 }
--- src/main/java/com/alc/diary/presentation/advice/RestControllerAdvice.java
+++ src/main/java/com/alc/diary/presentation/advice/RestControllerAdvice.java
@@ -1,13 +1,22 @@
 package com.alc.diary.presentation.advice;
 
 import com.alc.diary.domain.exception.DomainException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @org.springframework.web.bind.annotation.RestControllerAdvice
 public class RestControllerAdvice {
 
     @ExceptionHandler(value = DomainException.class)
-    void domainExceptionHandler(DomainException exception) {
+    public ResponseEntity<ErrorResponse> domainExceptionHandler(DomainException exception) {
+        ErrorResponse errorResponse =
+            new ErrorResponse(exception.getErrorModel().getCode(), exception.getErrorModel().getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> exceptionHandler(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorResponse.getDefault());
     }
 }
