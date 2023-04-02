c9fda23b63a2861f86b96d73f1649a6b24974b67
dev-taeyeong <dev.taeyeong@gmail.com>
2023-04-02 16:08:39 +0900

    rest controller advice 수정

--- src/main/java/com/alc/diary/domain/exception/DomainException.java
+++ src/main/java/com/alc/diary/domain/exception/DomainException.java
@@ -1,7 +1,9 @@
 package com.alc.diary.domain.exception;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @RequiredArgsConstructor
 public class DomainException extends RuntimeException {
     private final ErrorModel errorModel;
