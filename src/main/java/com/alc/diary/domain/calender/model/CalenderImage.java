cbf38293bda8ee7d2020e1977fa4e269640f9f73
tape22 <gjwjdlas@gmail.com>
2023-04-09 17:00:37 +0900

    feat: 캘린더 저장, 삭제, 수정 api 추가

--- src/main/java/com/alc/diary/domain/calender/model/CalenderImage.java
+++ src/main/java/com/alc/diary/domain/calender/model/CalenderImage.java
@@ -1,11 +1,18 @@
 package com.alc.diary.domain.calender.model;
 
+import lombok.Builder;
+
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 @Embeddable
+@Builder
 public class CalenderImage {
 
     @Column(name = "image_urls", length = 5000)
     private String urls;
+
+    public CalenderImage() {
+
+    }
 }
--- src/main/java/com/alc/diary/presentation/api/CalenderController.java
+++ src/main/java/com/alc/diary/presentation/api/CalenderController.java
@@ -1,5 +1,8 @@
 package com.alc.diary.presentation.api;
 
+import com.alc.diary.application.calender.CalenderService;
+import com.alc.diary.application.calender.dto.request.SaveCalenderRequest;
+import com.alc.diary.application.calender.dto.request.UpdateCalenderRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,19 +10,25 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/calender")
 public class CalenderController {
-    @PostMapping(value = "/")
-    public void save() {
 
+    private final CalenderService calenderService;
+
+    @PostMapping(value = "/")
+    public void save(@RequestBody SaveCalenderRequest request,
+                     @RequestAttribute Long userId) {
+        calenderService.save(request, userId);
     }
 
     @PutMapping(value = "/{calenderId}")
-    // 권한 체크
-    public void update(@PathVariable Long calenderId) {
-
+    public void update(@PathVariable Long calenderId,
+                       @RequestAttribute Long userId,
+                       @RequestBody UpdateCalenderRequest request) {
+        calenderService.update(calenderId, userId, request);
     }
 
     @DeleteMapping(value = "/{calenderId}")
-    public void delete(@PathVariable Long calenderId) {
-
+    public void delete(@PathVariable Long calenderId,
+                       @RequestAttribute Long userId) {
+        calenderService.delete(calenderId, userId);
     }
 }
