e829d42d4f375d91c47da59d095e1a0fd6506c0b
tape22 <gjwjdlas@gmail.com>
2023-04-09 16:59:10 +0900

    feat: 메인에서 캘린더 간편저장하는 api 추가

---
+++ src/main/java/com/alc/diary/application/calender/dto/request/SaveMainCalenderRequest.java
@@ -0,0 +1,12 @@
+package com.alc.diary.application.calender.dto.request;
+
+import com.alc.diary.domain.calender.model.DrinkModel;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record SaveMainCalenderRequest(
+        List<DrinkModel> drinkModels,
+        LocalDateTime drinkStartTime
+) {
+}
