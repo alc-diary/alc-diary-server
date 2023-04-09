2378346c0770ea6518589c7de45189854ddabf97
tape22 <gjwjdlas@gmail.com>
2023-04-09 14:50:06 +0900

    chore: swagger url이 auth filter에 걸리지 않도록 수정
    - restController만 인식하도록 수정

--- src/main/java/com/alc/diary/infrastructure/config/SwaggerConfiguration.java
+++ src/main/java/com/alc/diary/infrastructure/config/SwaggerConfiguration.java
@@ -2,7 +2,7 @@ package com.alc.diary.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import springfox.documentation.builders.PathSelectors;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
@@ -16,8 +16,7 @@ public class SwaggerConfiguration {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .build();
     }
 }
