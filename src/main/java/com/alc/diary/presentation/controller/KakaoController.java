ccb3fcad2fec83653bbb702714f7467ebe59b54c
taeyeong123 <taeyeong@cherground.com>
2023-04-10 15:12:25 +0900

    feature(auth): 카카오 로그인 테스트

--- src/main/java/com/alc/diary/presentation/controller/KakaoController.java
+++ src/main/java/com/alc/diary/presentation/controller/KakaoController.java
@@ -1,42 +1,28 @@
 package com.alc.diary.presentation.controller;
 
-import com.alc.diary.application.auth.dto.response.SocialLoginAppResponse;
-import com.alc.diary.application.auth.service.SocialLoginAppService;
-import com.alc.diary.infrastructure.external.client.feign.kakao.dto.response.KakaoLoginResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/kakao")
 @Controller
 public class KakaoController {
 
-    private final SocialLoginAppService socialLoginAppService;
-
     @GetMapping("/login")
     public String kakaoLogin() {
         return "kakao";
     }
 
-    @PostMapping("/login/success")
-    public String kakaoLoginSuccess(
-            @RequestBody KakaoLoginResponse request,
-            RedirectAttributes redirectAttributes
-    ) {
-        SocialLoginAppResponse response = socialLoginAppService.kakaoLogin(request);
-        redirectAttributes.addFlashAttribute("response", response);
-
-        return "redirect:/kakao/redirect";
-    }
-
     @GetMapping("/redirect")
-    public String kakaoRedirect() {
+    public String kakaoRedirect(
+            @RequestParam("accessToken") String accessToken,
+            @RequestParam("refreshToken") String refreshToken,
+            Model model
+    ) {
+        model.addAttribute("accessToken", accessToken);
+        model.addAttribute("refreshToken", refreshToken);
         return "kakao-redirect";
     }
 }
