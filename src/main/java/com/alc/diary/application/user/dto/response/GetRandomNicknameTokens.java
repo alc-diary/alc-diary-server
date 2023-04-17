7fc29b78325a5e2a03fbf9cd9ceafd9656f62df0
taeyeong123 <taeyeong@cherground.com>
2023-04-18 02:41:28 +0900

    admin main page

--- src/main/java/com/alc/diary/application/user/dto/response/GetRandomNicknameTokens.java
+++ src/main/java/com/alc/diary/application/user/dto/response/GetRandomNicknameTokens.java
@@ -3,7 +3,8 @@ package com.alc.diary.application.user.dto.response;
 import java.util.List;
 
 public record GetRandomNicknameTokens(
-        List<String> firstNicknameTokens,
-        List<String> secondNicknameTokens
+        List<NicknameTokenDto> firstNicknameTokens,
+        List<NicknameTokenDto> secondNicknameTokens
 ) {
+    public record NicknameTokenDto(Long id, String token) {}
 }
