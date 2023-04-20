0d8097051006d2a4628937044bb3e186bcd976e5
dev-taeyeong <dev.taeyeong@gmail.com>
2023-04-20 23:45:21 +0900

    구글 로그인 테스트

--- src/main/java/com/alc/diary/presentation/api/AuthApiController.java
+++ src/main/java/com/alc/diary/presentation/api/AuthApiController.java
@@ -6,11 +6,21 @@ import com.alc.diary.application.auth.dto.response.ReissueAccessTokenAppResponse
 import com.alc.diary.application.auth.dto.response.SocialLoginAppResponse;
 import com.alc.diary.application.auth.service.RefreshTokenAppService;
 import com.alc.diary.application.auth.service.SocialLoginAppService;
+import com.alc.diary.application.auth.strategy.KakaoLoginService;
+import com.alc.diary.application.auth.strategy.dto.SocialLoginStrategyResponse;
+import com.alc.diary.domain.user.enums.AgeRangeType;
+import com.alc.diary.domain.user.enums.GenderType;
 import com.alc.diary.domain.user.enums.SocialType;
+import com.alc.diary.infrastructure.external.client.feign.google.dto.response.GoogleUserInfoDto;
 import com.alc.diary.infrastructure.external.client.feign.kakao.dto.response.KakaoLoginResponse;
 import com.alc.diary.presentation.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+import static com.alc.diary.domain.user.enums.AgeRangeType.*;
+import static com.alc.diary.domain.user.enums.AgeRangeType.OVER_NINETY;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
@@ -42,8 +52,60 @@ public class AuthApiController {
 
     @PostMapping("/kakao/success")
     public ApiResponse<SocialLoginAppResponse> kakaoLogin(
-            @RequestBody KakaoLoginResponse request
+        @RequestBody KakaoLoginResponse request
     ) {
-        return ApiResponse.getSuccess(socialLoginAppService.kakaoLogin(request));
+        Optional<KakaoLoginResponse.KakaoAccount> kakaoAccountOptional = Optional.of(request.getKakaoAccount());
+        Optional<KakaoLoginResponse.KakaoAccount.Profile> profileOptional = kakaoAccountOptional.map(KakaoLoginResponse.KakaoAccount::getProfile);
+
+        String profileImageUrl = profileOptional.map(KakaoLoginResponse.KakaoAccount.Profile::getProfileImageUrl).orElse(null);
+        String email = kakaoAccountOptional.map(KakaoLoginResponse.KakaoAccount::getEmail).orElse(null);
+        GenderType gender = kakaoAccountOptional
+            .map(KakaoLoginResponse.KakaoAccount::getGender)
+            .map(s -> switch (s) {
+                case "male" -> GenderType.MALE;
+                case "female" -> GenderType.FEMALE;
+                default -> throw new IllegalArgumentException("Invalid gender: " + s);
+            })
+            .orElse(null);
+        AgeRangeType ageRange = kakaoAccountOptional.map(KakaoLoginResponse.KakaoAccount::getAgeRange)
+            .map(s -> switch (s) {
+                case "1~9" -> UNDER_TEN;
+                case "15_19" -> TEN_TO_FOURTEEN;
+                case "20~29" -> TWENTIES;
+                case "30~39" -> THIRTIES;
+                case "40~49" -> FORTIES;
+                case "50~59" -> FIFTIES;
+                case "60~69" -> SIXTIES;
+                case "70~79" -> SEVENTIES;
+                case "80~89" -> EIGHTIES;
+                case "90~" -> OVER_NINETY;
+                default -> throw new IllegalArgumentException("Invalid ageRange: " + s);
+            })
+            .orElse(null);
+
+        SocialLoginStrategyResponse socialLoginStrategyResponse = new SocialLoginStrategyResponse(
+            SocialType.KAKAO,
+            String.valueOf(request.getId()),
+            profileImageUrl,
+            email,
+            gender,
+            ageRange
+        );
+        return ApiResponse.getSuccess(socialLoginAppService.webSocialLogin(socialLoginStrategyResponse));
+    }
+
+    @PostMapping("/google/success")
+    public ApiResponse<SocialLoginAppResponse> googleLogin(
+        @RequestBody GoogleUserInfoDto request
+    ) {
+        SocialLoginStrategyResponse socialLoginStrategyResponse = new SocialLoginStrategyResponse(
+            SocialType.GOOGLE,
+            request.id(),
+            request.picture(),
+            request.email(),
+            null,
+            null
+        );
+        return ApiResponse.getSuccess(socialLoginAppService.webSocialLogin(socialLoginStrategyResponse));
     }
 }
