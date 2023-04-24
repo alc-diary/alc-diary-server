f39edf6b312cca868d4c8f1acc9b8ed762246703
정태영 <jeongtaeyeong@jeongtaeyeong-ui-MacBookPro.local>
2023-04-24 23:01:30 +0900

    end-point 수정

--- src/main/java/com/alc/diary/presentation/api/OnboardingApiController.java
+++ src/main/java/com/alc/diary/presentation/api/OnboardingApiController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/onboarding")
+@RequestMapping("/v1/onboarding")
 public class OnboardingApiController {
 
     private final OnboardingAppService onboardingAppService;
--- src/main/java/com/alc/diary/presentation/api/UserApiController.java
+++ src/main/java/com/alc/diary/presentation/api/UserApiController.java
@@ -2,6 +2,8 @@ package com.alc.diary.presentation.api;
 
 import com.alc.diary.application.user.UserAppService;
 import com.alc.diary.application.user.dto.request.CreateRandomNicknameTokenAppRequest;
+import com.alc.diary.application.user.dto.request.UpdateAlcoholLimitAndGoalAppRequest;
+import com.alc.diary.application.user.dto.request.UpdateUserProfileImageAppRequest;
 import com.alc.diary.application.user.dto.response.GetRandomNicknameAppResponse;
 import com.alc.diary.application.user.dto.response.GetUserInfoAppResponse;
 import com.alc.diary.presentation.dto.ApiResponse;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/v1/users")
 public class UserApiController {
 
     private final UserAppService userAppService;
@@ -33,5 +35,23 @@ public class UserApiController {
     @GetMapping("/nickname-token")
     public ApiResponse<GetRandomNicknameAppResponse> getRandomNickname() {
         return ApiResponse.getSuccess(userAppService.getRandomNickname());
+    }
+
+    @PutMapping("/profile-image")
+    public ApiResponse<Void> updateUserProfileImage(
+        @RequestAttribute Long userId,
+        @RequestBody UpdateUserProfileImageAppRequest request
+    ) {
+        userAppService.updateUserProfileImage(userId, request);
+        return ApiResponse.getSuccess();
+    }
+
+    @PutMapping("/alcohol-limit-and-goal")
+    public ApiResponse<Void> updateAlcoholLimitAndGoal(
+        @RequestAttribute Long userId,
+        @RequestBody UpdateAlcoholLimitAndGoalAppRequest request
+    ) {
+        userAppService.updateAlcoholLimitAndGoal(userId, request);
+        return ApiResponse.getSuccess();
     }
 }
