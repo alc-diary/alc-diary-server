9c3944447396a41c9a5c1b31c6733b3bd734a934
dev-taeyeong <dev.taeyeong@gmail.com>
2023-05-07 16:13:13 +0900

    feature(report): Implement getNumberOfDrinks(), getDaysOfDrinking() methods in Calendders class
    - Calenders.getNumberOfDrinks(): 마신 술 병 수 계산
    - Calenders.getDaysOfDrinking(): 술 마신 일 수 계산
    - 테스트 코드 추가

--- src/main/java/com/alc/diary/domain/calender/Calenders.java
+++ src/main/java/com/alc/diary/domain/calender/Calenders.java
@@ -1,10 +1,15 @@
 package com.alc.diary.domain.calender;
 
 import com.alc.diary.domain.calender.model.DrinkModel;
+import lombok.ToString;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+@ToString
 public class Calenders {
+
+    private static final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     private final List<Calender> calenders;
 
@@ -14,12 +19,15 @@ public class Calenders {
 
     public float getNumberOfDrinks() {
         return (float) calenders.stream()
-                        .flatMapToDouble(calender -> calender.getDrinkModels().stream()
-                                                             .mapToDouble(DrinkModel::getQuantity))
-                        .sum();
+                                .flatMapToDouble(calender -> calender.getDrinkModels().stream()
+                                                                     .mapToDouble(DrinkModel::getQuantity))
+                                .sum();
     }
 
-    public double getDaysOfDrinking() {
-
+    public int getDaysOfDrinking() {
+        return (int) calenders.stream()
+                              .map(calender -> dateFormat.format(calender.getDrinkStartDateTime()))
+                              .distinct()
+                              .count();
     }
 }
--- src/test/java/com/alc/diary/domain/calender/CalendersTest.java
+++ src/test/java/com/alc/diary/domain/calender/CalendersTest.java
@@ -1,7 +1,71 @@
 package com.alc.diary.domain.calender;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.alc.diary.domain.calender.enums.DrinkType;
+import com.alc.diary.domain.calender.model.CalenderImage;
+import com.alc.diary.domain.calender.model.DrinkModel;
+import com.alc.diary.domain.user.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CalendersTest {
 
+    private Calenders calenders;
+
+    @BeforeEach
+    void setUp() {
+        List<DrinkModel> drinkModels = List.of(
+                new DrinkModel(DrinkType.BEER, 1.5f),
+                new DrinkModel(DrinkType.SOJU, 3.0f)
+        );
+        calenders = new Calenders(IntStream.rangeClosed(1, 11)
+                                           .mapToObj(i -> Calender.builder()
+                                                                  .id((long) i)
+                                                                  .title("test title" + i)
+                                                                  .contents("test contents" + i)
+                                                                  .drinkStartDateTime(LocalDateTime.of(2023, 5, 7, 0, 0).plusHours(i * 12L))
+                                                                  .drinkEndDateTime(LocalDateTime.of(2023, 5, 7, 10, 10).plusHours(i * 12L))
+                                                                  .drinkModels(drinkModels)
+                                                                  .image(new CalenderImage("test image" + i))
+                                                                  .drinkCondition("😀")
+                                                                  .user(new User())
+                                                                  .build())
+                                           .collect(Collectors.toList())
+        );
+    }
+
+    @Test
+    void getNumberOfDrinks() {
+        assertThat(calenders.getNumberOfDrinks())
+                .isEqualTo(49.5f);
+    }
+
+    // 수정 예정
+    @Test
+    void getNumberOfDrinks_경곗값() {
+        Calenders calendersTest = new Calenders(Collections.emptyList());
+        assertThat(calendersTest.getNumberOfDrinks())
+                .isEqualTo(0.0f);
+    }
+
+    @Test
+    void getDaysOfDrinking() {
+        assertThat(calenders.getDaysOfDrinking())
+                .isEqualTo(6);
+    }
+
+    // 수정 예정
+    @Test
+    void getDaysOfDrinking_경곗값() {
+        Calenders calendersTest = new Calenders(Collections.emptyList());
+        assertThat(calendersTest.getDaysOfDrinking())
+                .isEqualTo(0);
+    }
 }
