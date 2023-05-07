0a4f3837ee0be73d74747daad824e6efd83f43e7
tape22 <gjwjdlas@gmail.com>
2023-05-07 14:26:15 +0900

    feat: 캘린더 요청 validate 추가

--- src/main/java/com/alc/diary/application/calender/dto/request/SaveCalenderRequest.java
+++ src/main/java/com/alc/diary/application/calender/dto/request/SaveCalenderRequest.java
@@ -3,20 +3,23 @@ package com.alc.diary.application.calender.dto.request;
 import com.alc.diary.domain.calender.model.DrinkModel;
 import com.alc.diary.domain.exception.InvalidRequestException;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public record SaveCalenderRequest(
         String title,
         String contents,
+        @NotNull(message = "음주 시작시간은 필수입니다.")
         LocalDateTime drinkStartDateTime,
+        @NotNull(message = "음주 끝나는 시간은 필수입니다.")
         LocalDateTime drinkEndDateTime,
+        @NotNull(message = "마신 음주 기록은 필수입니다.")
         List<DrinkModel> drinkModels,
         List<String> images,
         String drinkCondition
 
 ) {
-
     public SaveCalenderRequest {
         if (images.size() > 5) throw new InvalidRequestException();
     }
--- src/main/java/com/alc/diary/application/calender/dto/request/SaveMainCalenderRequest.java
+++ src/main/java/com/alc/diary/application/calender/dto/request/SaveMainCalenderRequest.java
@@ -2,11 +2,13 @@ package com.alc.diary.application.calender.dto.request;
 
 import com.alc.diary.domain.calender.model.DrinkModel;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public record SaveMainCalenderRequest(
+        @NotNull(message = "음주 기록은 필수입니다.")
         List<DrinkModel> drinkModels,
-        LocalDateTime drinkStartTime
+        LocalDateTime drinkStartDateTime
 ) {
 }
