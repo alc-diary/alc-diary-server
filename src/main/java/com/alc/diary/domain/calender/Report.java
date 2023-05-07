a122a08be245e82089f06ad9dd2d31de1bf2168f
dev-taeyeong <dev.taeyeong@gmail.com>
2023-05-07 15:36:35 +0900

    resolve conflict

---
+++ src/main/java/com/alc/diary/domain/calender/Report.java
@@ -0,0 +1,17 @@
+package com.alc.diary.domain.calender;
+
+public class Report {
+
+    private final Calenders calenders;
+
+    public Report(Calenders calenders) {
+        this.calenders = calenders;
+    }
+
+    public void getNumberOfDrinks() {
+    }
+
+    public void getDaysOfDrinking() {
+
+    }
+}
