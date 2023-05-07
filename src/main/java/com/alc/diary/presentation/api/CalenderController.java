af1f3cc44fd57b989d745e24f10ba739e1f87a4e
tape22 <gjwjdlas@gmail.com>
2023-05-07 18:27:01 +0900

    feat: 캘린더 월별/일별 조회 추가

--- src/main/java/com/alc/diary/presentation/api/CalenderController.java
+++ src/main/java/com/alc/diary/presentation/api/CalenderController.java
@@ -2,8 +2,11 @@ package com.alc.diary.presentation.api;
 
 import com.alc.diary.application.calender.CalenderService;
 import com.alc.diary.application.calender.dto.request.SaveCalenderRequest;
+import com.alc.diary.application.calender.dto.request.SearchCalenderRequest;
 import com.alc.diary.application.calender.dto.request.UpdateCalenderRequest;
 import com.alc.diary.application.calender.dto.response.FindCalenderDetailResponse;
+import com.alc.diary.application.calender.dto.response.SearchCalenderResponse;
+import com.alc.diary.domain.calender.enums.QueryType;
 import com.alc.diary.presentation.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
@@ -22,12 +25,12 @@ public class CalenderController {
     }
 
     @GetMapping(value = "search")
-    public void search(
-            @RequestParam(value = "month") Integer month,
-            @RequestParam(value = "day", required = false) Integer day,
+    public ApiResponse<SearchCalenderResponse> search(
+            @RequestParam(value = "query", required = false) String query,
+            @RequestParam(value = "date", required = false) String date,
             @RequestAttribute Long userId
     ) {
-        // month, day
+        return ApiResponse.getSuccess(calenderService.search(new SearchCalenderRequest(userId, QueryType.valueOf(query), date)));
     }
 
     @PostMapping(value = "")
