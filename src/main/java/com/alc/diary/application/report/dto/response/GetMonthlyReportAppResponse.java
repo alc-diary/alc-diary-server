65903a816ab247dc9bb494a5bf6bf265b0dc4e1d
dev-taeyeong <dev.taeyeong@gmail.com>
2023-05-07 18:17:18 +0900

    feature(report): Implement getMostDayOfWeek api 추가

--- src/main/java/com/alc/diary/application/report/dto/response/GetMonthlyReportAppResponse.java
+++ src/main/java/com/alc/diary/application/report/dto/response/GetMonthlyReportAppResponse.java
@@ -2,10 +2,14 @@ package com.alc.diary.application.report.dto.response;
 
 import com.alc.diary.domain.calender.enums.DrinkType;
 
+import java.time.DayOfWeek;
+import java.util.List;
+
 public record GetMonthlyReportAppResponse(
 
         float numberOfDrinks,
         int daysOfDrinking,
-        DrinkType mostDrunkAlcoholType
+        DrinkType mostDrunkAlcoholType,
+        List<DayOfWeek> mostDayOfWeek
 ) {
 }
