782d3f6768820c6a7c7a62d6a1a1d96835b3c520
dev-taeyeong <dev.taeyeong@gmail.com>
2023-05-10 23:07:38 +0900

    월간 통계 API 도메인 객체와 DTO 분리

---
+++ src/main/java/com/alc/diary/application/report/dto/response/DrinkingDaySummaryDto.java
@@ -0,0 +1,16 @@
+package com.alc.diary.application.report.dto.response;
+
+import com.alc.diary.domain.calender.DrinkingDaySummary;
+
+import java.time.DayOfWeek;
+
+public record DrinkingDaySummaryDto(
+
+        DayOfWeek dayOfWeek,
+        int totalOccurrence
+) {
+
+    public DrinkingDaySummaryDto(DrinkingDaySummary drinkingDaySummary) {
+        this(drinkingDaySummary.getDayOfWeek(), drinkingDaySummary.getTotalOccurrence());
+    }
+}
