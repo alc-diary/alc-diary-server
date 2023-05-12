3062f45734df76ff942a1284dc20656b79ea22ef
taeyeong123 <taeyeong@cherground.com>
2023-05-12 17:48:26 +0900

    DrinkModel getter에 @JsonIgnore 추가

--- src/main/java/com/alc/diary/domain/calender/model/DrinkModel.java
+++ src/main/java/com/alc/diary/domain/calender/model/DrinkModel.java
@@ -2,6 +2,7 @@ package com.alc.diary.domain.calender.model;
 
 
 import com.alc.diary.domain.calender.enums.DrinkType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 @Builder
@@ -13,10 +14,12 @@ public class DrinkModel {
     private DrinkType type;
     private float quantity;
 
+    @JsonIgnore
     public int getTotalPrice() {
         return (int) quantity * type.getPrice();
     }
 
+    @JsonIgnore
     public int getTotalCalorie() {
         return (int) quantity * type.getCalorie();
     }
