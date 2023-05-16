7092119aa07227c3ea485484bc020b8f863a38df
dev-taeyeong <dev.taeyeong@gmail.com>
2023-05-17 01:17:55 +0900

    timezone test

--- src/main/java/com/alc/diary/presentation/api/TestApiController.java
+++ src/main/java/com/alc/diary/presentation/api/TestApiController.java
@@ -1,18 +1,35 @@
 package com.alc.diary.presentation.api;
 
+import com.alc.diary.domain.calender.Calender;
+import com.alc.diary.domain.calender.repository.CalenderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @Slf4j
 @RequiredArgsConstructor
+@RequestMapping("/test")
 @RestController
 public class TestApiController {
 
-    @GetMapping("/test")
+    private final CalenderRepository calenderRepository;
+
+    @GetMapping
     public void test(@RequestParam String user) {
         log.info("user: {}", user);
+    }
+
+    @GetMapping("/timezone")
+    public LocalDateTime timezone() {
+        return LocalDateTime.now();
+    }
+
+    @GetMapping("/calender/{calenderId}")
+    public Calender getCalender(
+            @PathVariable Long calenderId
+    ) {
+        return calenderRepository.getCalenderById(calenderId).orElse(null);
     }
 }
