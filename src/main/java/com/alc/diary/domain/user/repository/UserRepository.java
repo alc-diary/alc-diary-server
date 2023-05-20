9c9845a8703462328a396aac8d5d93b12f7e6bbc
dev-taeyeong <dev.taeyeong@gmail.com>
2023-05-20 12:32:10 +0900

    feature(user): 온보딩이 끝났는지 여부 조회 API 추가

--- src/main/java/com/alc/diary/domain/user/repository/UserRepository.java
+++ src/main/java/com/alc/diary/domain/user/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.alc.diary.domain.user.repository;
 
 import com.alc.diary.domain.user.User;
 import com.alc.diary.domain.user.enums.SocialType;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 import java.util.Optional;
@@ -9,6 +10,11 @@ import java.util.Optional;
 public interface UserRepository extends Repository<User, Long>, CustomUserRepository {
 
     Optional<User> findById(Long id);
+
+    @Query("select u " +
+           "from User u " +
+           "where u.id = :id")
+    Optional<User> findByIdIgnoringWhere(Long id);
 
     User save(User user);
 
