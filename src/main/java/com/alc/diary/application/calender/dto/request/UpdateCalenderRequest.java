c518276e8b338a91b965ee8b6947cb43ef47e7b6
dev-taeyeong <dev.taeyeong@gmail.com>
2023-06-06 23:37:38 +0900

    drinkModels, images 없을 경우 null 대신 빈 배열로 클라이언트와 통신 하도록 수정

--- src/main/java/com/alc/diary/application/calender/dto/request/UpdateCalenderRequest.java
+++ src/main/java/com/alc/diary/application/calender/dto/request/UpdateCalenderRequest.java
@@ -2,6 +2,7 @@ package com.alc.diary.application.calender.dto.request;
 
 import com.alc.diary.domain.calender.model.DrinkModel;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -10,7 +11,9 @@ public record UpdateCalenderRequest(
         String contents,
         LocalDateTime drinkStartDateTime,
         LocalDateTime drinkEndDateTime,
+        @NotNull(message = "마신 음주 기록은 null을 허용하지 않습니다.")
         List<DrinkModel> drinkModels,
+        @NotNull(message = "프로필 이미지는 null을 허용하지 않습니다.")
         List<String> images,
         String drinkCondition
 ) {
