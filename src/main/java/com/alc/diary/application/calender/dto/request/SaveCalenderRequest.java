ab60e964d40a1d9a72fc15b39c31fe329d945f9e
dev-taeyeong <dev.taeyeong@gmail.com>
2023-06-06 23:37:38 +0900

    drinkModels, images 없을 경우 null 대신 빈 배열로 클라이언트와 통신 하도록 수정

--- src/main/java/com/alc/diary/application/calender/dto/request/SaveCalenderRequest.java
+++ src/main/java/com/alc/diary/application/calender/dto/request/SaveCalenderRequest.java
@@ -2,10 +2,12 @@ package com.alc.diary.application.calender.dto.request;
 
 import com.alc.diary.domain.calender.model.DrinkModel;
 import com.alc.diary.domain.exception.InvalidRequestException;
+import org.springframework.util.CollectionUtils;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 
 public record SaveCalenderRequest(
@@ -16,14 +18,14 @@ public record SaveCalenderRequest(
         LocalDateTime drinkStartDateTime,
         @NotNull(message = "음주 끝나는 시간은 필수입니다.")
         LocalDateTime drinkEndDateTime,
-        @NotNull(message = "마신 음주 기록은 필수입니다.")
-        @NotEmpty(message = "음주 기록은 필수입니다.")
+        @NotNull(message = "마신 음주 기록은 null을 허용하지 않습니다.")
         List<DrinkModel> drinkModels,
+        @NotNull(message = "프로필 이미지는 null을 허용하지 않습니다.")
         List<String> images,
         String drinkCondition
 
 ) {
     public SaveCalenderRequest {
-        if (images.size() > 5) throw new InvalidRequestException();
+        if (images != null && images.size() > 5) throw new InvalidRequestException();
     }
 }
