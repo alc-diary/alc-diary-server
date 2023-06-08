672ea11b9b95bb5344756eed93cfded0b0d2062e
dev-taeyeong <dev.taeyeong@gmail.com>
2023-06-08 20:03:02 +0900

    온보딩 인증 쿼리 수정

--- src/main/java/com/alc/diary/presentation/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/alc/diary/presentation/filter/JwtAuthenticationFilter.java
@@ -57,12 +57,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
             if (isOnboardingEndpoint(path)) {
                 if (path.equals("/v1/onboarding/is-onboarding-done")) {
-                    if (!userRepository.existsById(userId)) {
+                    if (userRepository.findByIdAndDeletedAtIsNull(userId).isEmpty()) {
                         throw new DomainException(UserError.USER_NOT_FOUND);
                     }
                 } else {
-                    log.info("userId: {}", userId);
-                    log.info("isEmpty(): {}", userRepository.findByIdAndStatusEqualsOnboarding(userId).isEmpty());
                     if (userRepository.findByIdAndStatusEqualsOnboarding(userId).isEmpty()) {
                         throw new DomainException(UserError.USER_NOT_FOUND);
                     }
