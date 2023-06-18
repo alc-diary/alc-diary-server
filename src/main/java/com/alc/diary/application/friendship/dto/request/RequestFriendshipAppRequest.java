4a7bd09ca04fadc6bee13d55ccc79df2b426e5f8
dev-taeyeong <dev.taeyeong@gmail.com>
2023-06-18 12:46:38 +0900

    validation 로직 생성

--- src/main/java/com/alc/diary/application/friendship/dto/request/RequestFriendshipAppRequest.java
+++ src/main/java/com/alc/diary/application/friendship/dto/request/RequestFriendshipAppRequest.java
@@ -1,7 +1,9 @@
 package com.alc.diary.application.friendship.dto.request;
 
 public record RequestFriendshipAppRequest(
+
         String targetNickname,
+        String alias,
         String message
 ) {
 }
---
+++ src/test/java/com/alc/diary/application/friendship/FriendshipAppServiceTest.java
@@ -0,0 +1,8 @@
+package com.alc.diary.application.friendship;
+
+import org.junit.jupiter.api.Test;
+
+class FriendshipAppServiceTest {
+
+
+}
