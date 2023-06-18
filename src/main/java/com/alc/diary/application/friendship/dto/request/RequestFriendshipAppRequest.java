6888f9c6a6c2c48197e1ed88a296b98a5757d7da
dev-taeyeong <dev.taeyeong@gmail.com>
2023-06-18 16:55:39 +0900

    닉네임 검증 정규식 수정

--- src/main/java/com/alc/diary/application/friendship/dto/request/RequestFriendshipAppRequest.java
+++ src/main/java/com/alc/diary/application/friendship/dto/request/RequestFriendshipAppRequest.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.Size;
 public record RequestFriendshipAppRequest(
 
         @NotNull(message = "닉네임은 필수입니다.")
-        @Pattern(regexp = "^[a-zA-Z0-9가-힣]*$", message = "닉네임은 한글, 영어 대소문자, 숫자만 가능합니다.")
+        @Pattern(regexp = "^[a-zA-Z0-9가-힣]+$", message = "닉네임은 한글, 영어 대소문자, 숫자만 가능합니다.")
         String targetNickname,
         String alias,
         @Size(max = 100, message = "메시지는 100자 이하여야 합니다.") String message
