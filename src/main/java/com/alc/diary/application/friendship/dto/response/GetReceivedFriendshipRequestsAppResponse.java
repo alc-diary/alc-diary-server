9c34a90e192aa4764f32470f3cc0c1df1e78cdb9
dev-taeyeong <dev.taeyeong@gmail.com>
2023-06-18 15:48:13 +0900

    swagger 설정 추가

--- src/main/java/com/alc/diary/application/friendship/dto/response/GetReceivedFriendshipRequestsAppResponse.java
+++ src/main/java/com/alc/diary/application/friendship/dto/response/GetReceivedFriendshipRequestsAppResponse.java
@@ -6,7 +6,9 @@ import com.alc.diary.domain.friendship.enums.FriendshipStatus;
 public record GetReceivedFriendshipRequestsAppResponse(
         long requestId,
         long fromUserId,
+        String fromUserNickname,
         long toUserId,
+        String toUserNickname,
         FriendshipStatus status,
         String message
 ) {
@@ -15,7 +17,9 @@ public record GetReceivedFriendshipRequestsAppResponse(
         return new GetReceivedFriendshipRequestsAppResponse(
                 friendship.getId(),
                 friendship.getFromUser().getId(),
+                friendship.getFromUser().getNickname(),
                 friendship.getToUser().getId(),
+                friendship.getToUser().getNickname(),
                 friendship.getStatus(),
                 friendship.getMessage()
         );
