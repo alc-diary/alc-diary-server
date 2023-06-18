1f9cbf9cb178233a4ace14cd7c28d3e490106faa
dev-taeyeong <dev.taeyeong@gmail.com>
2023-06-18 12:53:31 +0900

    Controller validation 로직 추가

--- src/main/java/com/alc/diary/application/user/dto/request/UpdateAlcoholLimitAndGoalAppRequest.java
+++ src/main/java/com/alc/diary/application/user/dto/request/UpdateAlcoholLimitAndGoalAppRequest.java
@@ -2,9 +2,11 @@ package com.alc.diary.application.user.dto.request;
 
 import com.alc.diary.domain.user.enums.AlcoholType;
 
+import javax.validation.constraints.NotNull;
+
 public record UpdateAlcoholLimitAndGoalAppRequest(
-    float newPersonalAlcoholLimit,
-    int newNonAlcoholGoal,
-    AlcoholType newAlcoholType
+    @NotNull(message = "주량은 필수입니다.") float newPersonalAlcoholLimit,
+    @NotNull(message = "금주 목표는 필수입니다.") int newNonAlcoholGoal,
+    @NotNull(message = "주종은 필수입니다.") AlcoholType newAlcoholType
 ) {
 }
