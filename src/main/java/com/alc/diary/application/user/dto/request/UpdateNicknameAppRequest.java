a35968876d5cca56d5ef0673ec73e4e368336a7e
dev-taeyeong <dev.taeyeong@gmail.com>
2023-06-18 16:55:39 +0900

    닉네임 검증 정규식 수정

--- src/main/java/com/alc/diary/application/user/dto/request/UpdateNicknameAppRequest.java
+++ src/main/java/com/alc/diary/application/user/dto/request/UpdateNicknameAppRequest.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.Pattern;
 public record UpdateNicknameAppRequest(
 
         @NotNull(message = "닉네임은 필수입니다.")
-        @Pattern(regexp = "^[a-zA-Z0-9가-힣]*$", message = "닉네임은 한글, 영어 대소문자, 숫자만 가능합니다.")
+        @Pattern(regexp = "^[a-zA-Z0-9가-힣]+$", message = "닉네임은 한글, 영어 대소문자, 숫자만 가능합니다.")
         String newNickname
 ) {
 }
