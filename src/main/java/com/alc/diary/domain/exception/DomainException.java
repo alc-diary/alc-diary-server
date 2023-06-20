3c07f1183f76279601ac46763d13374ba752d8a9
taeyeong123 <taeyeong@cherground.com>
2023-06-20 15:22:45 +0900

    DomainException 수정

--- src/main/java/com/alc/diary/domain/exception/DomainException.java
+++ src/main/java/com/alc/diary/domain/exception/DomainException.java
@@ -4,12 +4,26 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class DomainException extends RuntimeException {
+
     private final ErrorModel errorModel;
+    private final String additionalInfo;
+
+    public DomainException(ErrorModel errorModel) {
+        this(errorModel, null);
+    }
+
+    public DomainException(ErrorModel errorModel, String additionalInfo) {
+        super(errorModel.getMessage());
+        this.errorModel = errorModel;
+        this.additionalInfo = additionalInfo;
+    }
 
     @Override
     public String getMessage() {
+        if (additionalInfo != null) {
+            return errorModel.getMessage() + " | " + additionalInfo;
+        }
         return errorModel.getMessage();
     }
 }
