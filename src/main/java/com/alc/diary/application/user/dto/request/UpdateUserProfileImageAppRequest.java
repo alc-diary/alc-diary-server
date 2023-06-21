2e0c7702d8dff6164ab7974f3807df3b1df092c6
dev-taeyeong <dev.taeyeong@gmail.com>
2023-06-21 11:55:26 +0900

    유저 프로필 필수 validation 수정

--- src/main/java/com/alc/diary/application/user/dto/request/UpdateUserProfileImageAppRequest.java
+++ src/main/java/com/alc/diary/application/user/dto/request/UpdateUserProfileImageAppRequest.java
@@ -1,7 +1,9 @@
 package com.alc.diary.application.user.dto.request;
 
+import javax.validation.constraints.Size;
+
 public record UpdateUserProfileImageAppRequest(
 
-        String newProfileImage
+        @Size(max = 500, message = "프로필 이미지 ULR은 500자 이하여야 합니다.") String newProfileImage
 ) {
 }
