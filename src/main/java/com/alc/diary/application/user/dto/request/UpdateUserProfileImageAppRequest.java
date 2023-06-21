51ff3166cd4f4375a7db6eac25404aad02e1536b
taeyeong123 <taeyeong@cherground.com>
2023-06-21 11:54:09 +0900

    유저 프로필 필수 validation 제거

--- src/main/java/com/alc/diary/application/user/dto/request/UpdateUserProfileImageAppRequest.java
+++ src/main/java/com/alc/diary/application/user/dto/request/UpdateUserProfileImageAppRequest.java
@@ -1,9 +1,7 @@
 package com.alc.diary.application.user.dto.request;
 
-import javax.validation.constraints.NotNull;
-
 public record UpdateUserProfileImageAppRequest(
 
-        @NotNull(message = "프로필 이미지 URL은 필수입니다.") String newProfileImage
+        String newProfileImage
 ) {
 }
