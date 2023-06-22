113dcba42e8b8c9e09b894a127d3066870bfd8b6
dev-taeyeong <dev.taeyeong@gmail.com>
2023-06-23 02:15:17 +0900

    feature(friendship): 친구 요청 취소 API 추가, 친구 요청 수락 API에 별칭 입력 추가

--- src/main/java/com/alc/diary/domain/friendship/error/FriendshipError.java
+++ src/main/java/com/alc/diary/domain/friendship/error/FriendshipError.java
@@ -12,13 +12,14 @@ public enum FriendshipError implements ErrorModel {
     INVALID_REQUEST("FS_E0001", "The request is invalid. Please check your input and try again."),
     ALREADY_SENT_REQUEST("FS_E0002", "A friendship request has already been sent to this user."),
     NO_PERMISSION_TO_ACCEPT("FS_E0003", "You do not have permission to accept this friendship request."),
-    NO_PERMISSION_TO_DELETE("FS_E0004", "You do not have permission to delete this friend."),
+    NO_PERMISSION_TO_DELETE("FS_E0004", "You do not have permission to delete this friendship."),
     FROM_USER_NULL("FS_E0005", "FromUser cannot be null."),
     TO_USER_NULL("FS_E0006", "ToUser cannot be null."),
     MESSAGE_LENGTH_EXCEEDED("FS_E0007", "Message length cannot exceed 100 characters."),
     USER_ALIAS_LENGTH_EXCEEDED("FS_E0008", "User alias length cannot exceed 30 characters."),
     NO_PERMISSION_TO_DECLINE("FS_E0009", "You do not have permission to decline this friendship."),
     NO_PERMISSION("FS_E0010", "You do not have permission to this friendship."),
+    NO_PERMISSION_TO_CANCEL("FS_E0011", "You do not have permission to cancel this friendship request.")
     ;
 
     private final String code;
