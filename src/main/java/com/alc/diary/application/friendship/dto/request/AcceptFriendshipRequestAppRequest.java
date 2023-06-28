c2ac92442c37fbf90807b6f12e75de2cb402b3c0
dev-taeyeong <dev.taeyeong@gmail.com>
2023-06-28 19:03:48 +0900

    refactoring

--- src/main/java/com/alc/diary/application/friendship/dto/request/AcceptFriendshipRequestAppRequest.java
+++ src/main/java/com/alc/diary/application/friendship/dto/request/AcceptFriendshipRequestAppRequest.java
@@ -2,6 +2,6 @@ package com.alc.diary.application.friendship.dto.request;
 
 public record AcceptFriendshipRequestAppRequest(
 
-        String alias
+        String friendLabel
 ) {
 }
--- src/main/java/com/alc/diary/application/friendship/dto/response/GetPendingRequestsAppResponse.java
+++ src/main/java/com/alc/diary/application/friendship/dto/response/GetPendingRequestsAppResponse.java
@@ -1,5 +1,6 @@
 package com.alc.diary.application.friendship.dto.response;
 
+import com.alc.diary.domain.friendship.FriendRequest;
 import com.alc.diary.domain.friendship.Friendship;
 
 import java.util.List;
@@ -12,15 +13,4 @@ public record GetPendingRequestsAppResponse(
         String requestReceiverNickname,
         String receiverProfileImageUrl
 ) {
-
-    public static List<GetPendingRequestsAppResponse> from(List<Friendship> friendships) {
-        return friendships.stream()
-                .map(friendship -> new GetPendingRequestsAppResponse(
-                        friendship.getId(),
-                        friendship.getToUser().getId(),
-                        friendship.getToUser().getNickname(),
-                        friendship.getToUser().getProfileImage()
-                ))
-                .collect(Collectors.toList());
-    }
 }
