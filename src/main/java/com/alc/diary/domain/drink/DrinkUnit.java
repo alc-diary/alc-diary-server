7c2d5359214760aa24a7f61b3af7b7ceb36e809b
dev-taeyeong <dev.taeyeong@gmail.com>
2023-07-05 18:36:05 +0900

    feature(calendar): version 2

---
+++ src/main/java/com/alc/diary/domain/drink/DrinkUnit.java
@@ -0,0 +1,18 @@
+package com.alc.diary.domain.drink;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum DrinkUnit {
+
+    BOTTLE("병"),
+    GLASS("잔"),
+    CAN("캔"),
+    ML_500("500mL"),
+    PITCHER("피처"),
+    ;
+
+    private final String displayName;
+}
