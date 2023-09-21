935742ebc8acd316826c3d023ed3fd6fcea6f3a5
dev-taeyeong <dev.taeyeong@gmail.com>
2023-09-22 00:45:09 +0900

    #69 - reissueAccessTokenTest 작성

--- src/test/java/com/alc/diary/presentation/api/AuthApiControllerTest.java
+++ src/test/java/com/alc/diary/presentation/api/AuthApiControllerTest.java
@@ -1,6 +1,7 @@
 package com.alc.diary.presentation.api;
 
 import com.alc.diary.application.auth.dto.request.SocialLoginAppRequest;
+import com.alc.diary.application.auth.dto.response.ReissueAccessTokenAppResponse;
 import com.alc.diary.application.auth.dto.response.SocialLoginAppResponse;
 import com.alc.diary.application.auth.service.RefreshTokenAppService;
 import com.alc.diary.application.auth.service.SocialLoginAppService;
@@ -15,7 +16,9 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -35,17 +38,16 @@ class AuthApiControllerTest {
     private MockMvc mvc;
 
     @Test
-    void socialLogin() throws Exception {
+    void socialLoginTest() throws Exception {
         // given
         SocialType socialType = SocialType.KAKAO;
         String socialAccessToken = "test-token";
         String userAgent = "ios";
-        SocialLoginAppRequest request = new SocialLoginAppRequest(socialType, socialAccessToken);
 
         String accessToken = "test_access-token";
         String refreshToken = "test_refresh-token";
 
-        given(socialLoginAppService.login(request, userAgent))
+        given(socialLoginAppService.login(any(), any()))
                 .willReturn(new SocialLoginAppResponse(accessToken, refreshToken));
 
         // when & then
@@ -60,7 +62,26 @@ class AuthApiControllerTest {
                 .andExpect(jsonPath("$.message").value("success"))
                 .andExpect(jsonPath("$.data.accessToken").value(accessToken))
                 .andExpect(jsonPath("$.data.refreshToken").value(refreshToken));
+        then(socialLoginAppService).should().login(any(), any());
     }
 
+    @Test
+    void reissueAccessTokenTest() throws Exception {
+        // given
+        String accessToken = "new_access-token";
+        String refreshToken = "new_refresh-token";
+        given(refreshTokenAppService.reissueToken(any())).willReturn(new ReissueAccessTokenAppResponse(accessToken, refreshToken));
 
+        // when & then
+        mvc.perform(post("/v1/auth/access-token/reissue")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"refreshToken\": \"test refresh token\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value(200))
+                .andExpect(jsonPath("$.code").value("S0000"))
+                .andExpect(jsonPath("$.message").value("success"))
+                .andExpect(jsonPath("$.data.newAccessToken").value(accessToken))
+                .andExpect(jsonPath("$.data.newRefreshToken").value(refreshToken));
+        then(refreshTokenAppService).should().reissueToken(any());
+    }
 }
