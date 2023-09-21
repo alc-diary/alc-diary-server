628bad62978de6ab97d036546008432493e47adb
dev-taeyeong <dev.taeyeong@gmail.com>
2023-09-21 22:43:41 +0900

    #69 - 애노테이션 설정 클래스로 이동

--- src/main/java/com/alc/diary/AlcDiaryApplication.java
+++ src/main/java/com/alc/diary/AlcDiaryApplication.java
@@ -6,9 +6,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
-@EnableCaching
-@EnableFeignClients
 @SpringBootApplication
 public class AlcDiaryApplication {
 
--- src/main/java/com/alc/diary/config/FeignConfig.java
+++ src/main/java/com/alc/diary/config/FeignConfig.java
@@ -2,9 +2,11 @@ package com.alc.diary.config;
 
 import com.alc.diary.infrastructure.external.client.feign.FeignCustomLogger;
 import feign.Logger;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@EnableFeignClients
 @Configuration
 public class FeignConfig {
 
