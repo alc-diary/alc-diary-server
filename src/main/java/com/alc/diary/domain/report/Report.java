0a1b87fbce537364c4bac58006c555953e2a3f5a
dev-taeyeong <dev.taeyeong@gmail.com>
2023-10-01 18:57:49 +0900

    fix: 월간 리포트 API 수정
    술에 사용한 돈을 국밥 갯수로 변환하는 로직 수정

--- src/main/java/com/alc/diary/domain/report/Report.java
+++ src/main/java/com/alc/diary/domain/report/Report.java
@@ -13,44 +13,44 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class Report {
 
-   private static final int CALORIES_BURN_PER_HOUR_RUNNING = 580;
-   private static final int PRICE_OF_A_BOWL_OF_RICE_SOUP = 8000;
+    private static final int CALORIES_BURN_PER_HOUR_RUNNING = 580;
+    private static final int PRICE_OF_A_BOWL_OF_RICE_SOUP = 8000;
 
-   public final Calendars calendars;
+    public final Calendars calendars;
 
-   public int totalDaysDrinking() {
-       return calendars.totalDaysDrinking();
-   }
+    public int totalDaysDrinking() {
+        return calendars.totalDaysDrinking();
+    }
 
-   public int totalSpentOnDrinks() {
-       return calendars.calculateTotalSpent();
-   }
+    public int totalSpentOnDrinks() {
+        return calendars.calculateTotalSpent();
+    }
 
-   public int totalCalories() {
-       return calendars.calculateTotalCalories();
-   }
+    public int totalCalories() {
+        return calendars.calculateTotalCalories();
+    }
 
-   public float totalQuantity() {
-       return calendars.calculateTotalQuantity();
-   }
+    public float totalQuantity() {
+        return calendars.calculateTotalQuantity();
+    }
 
-   public int totalRunningTimeToBurnCalories() {
-       return Math.round((float) totalCalories() / CALORIES_BURN_PER_HOUR_RUNNING);
-   }
+    public int totalRunningTimeToBurnCalories() {
+        return Math.round((float) totalCalories() / CALORIES_BURN_PER_HOUR_RUNNING);
+    }
 
-   public int riceSoupEquivalent() {
-       return Math.round((float) totalCalories() / PRICE_OF_A_BOWL_OF_RICE_SOUP);
-   }
+    public int riceSoupEquivalent() {
+        return Math.round((float) totalSpentOnDrinks() / PRICE_OF_A_BOWL_OF_RICE_SOUP);
+    }
 
-   public Optional<DrinkType> mostConsumedDrink() {
-       return calendars.mostConsumedDrink();
-   }
+    public Optional<DrinkType> mostConsumedDrink() {
+        return calendars.mostConsumedDrink();
+    }
 
-   public Optional<DayOfWeek> mostFrequentDrinkingDay() {
-       return calendars.mostFrequentDrinkingDay();
-   }
+    public Optional<DayOfWeek> mostFrequentDrinkingDay() {
+        return calendars.mostFrequentDrinkingDay();
+    }
 
-   public Optional<ZonedDateTime> getLastDrinkingDateTime() {
-       return calendars.getLastDrinkingDateTime();
-   }
+    public Optional<ZonedDateTime> getLastDrinkingDateTime() {
+        return calendars.getLastDrinkingDateTime();
+    }
 }
