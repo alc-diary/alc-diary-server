481909cdc621c20a5f6787b18585beaad27f68de
dev-taeyeong <dev.taeyeong@gmail.com>
2024-01-16 23:56:12 +0900

    temp: 알림 배치 테스트 (추후 삭제 될 코드)

--- src/main/java/com/alc/diary/application/notification/NotificationBatchService.java
+++ src/main/java/com/alc/diary/application/notification/NotificationBatchService.java
@@ -10,8 +10,8 @@ public class NotificationBatchService {
 
     private final NotificationService notificationService;
 
-    @Scheduled(cron = "0 * 10 * * MON,TUE")
+    @Scheduled(cron = "0 * 0 * * MON,TUE,WED")
     public void batchTest() {
-        notificationService.sendFcm(120, "zz", "테스트", "TEST");
+        notificationService.sendFcm(120, "술렁술렁", "안녕! 좋은 아침이야 :)\n혹시 어제 술 마시지 않았니? 얼마나 마셨어?", "TEMPORARY");
     }
 }
