ce735f8fe52e066d399278c0c13c66a8994633d0
dev-taeyeong <dev.taeyeong@gmail.com>
2024-02-01 18:41:35 +0900

    wip: admin api cors 허용 (임시)

--- src/main/java/com/alc/diary/application/admin/AdminCalendarService.java
+++ src/main/java/com/alc/diary/application/admin/AdminCalendarService.java
@@ -9,7 +9,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Service
