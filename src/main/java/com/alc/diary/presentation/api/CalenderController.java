87bae9daf688117b8f95eebfc974555cb2d7c74a
dev-taeyeong <dev.taeyeong@gmail.com>
2024-02-17 22:05:43 +0900

    chore: CalendarController deprecated

--- src/main/java/com/alc/diary/presentation/api/CalenderController.java
+++ src/main/java/com/alc/diary/presentation/api/CalenderController.java
@@ -13,6 +13,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
+@Deprecated
 @RestController
 @RequiredArgsConstructor
 public class CalenderController {
