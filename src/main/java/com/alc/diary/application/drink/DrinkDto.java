ff6fc55d8c786f59a20befe418713a900e3d06f4
dev-taeyeong <dev.taeyeong@gmail.com>
2024-02-27 00:58:33 +0900

    feat: DrinkDto에 추가적인 정보 제공

--- src/main/java/com/alc/diary/application/drink/DrinkDto.java
+++ src/main/java/com/alc/diary/application/drink/DrinkDto.java
@@ -8,6 +8,8 @@ public record DrinkDto(
         long id,
         long categoryId,
         String name,
+        long creatorId,
+        boolean isPublic,
         DrinkType type) {
 
     public static DrinkDto fromDomainModel(Drink drink) {
@@ -15,6 +17,8 @@ public record DrinkDto(
                 drink.getId(),
                 drink.getCategoryId(),
                 drink.getName(),
+                drink.getCreatorId(),
+                drink.getIsPublic(),
                 drink.getType()
         );
     }
