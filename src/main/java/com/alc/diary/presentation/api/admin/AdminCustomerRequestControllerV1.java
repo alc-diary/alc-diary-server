ab28d41d28df9bcecd5b6ea637367fbbf6e14359
dev-taeyeong <dev.taeyeong@gmail.com>
2024-03-24 20:34:41 +0900

    feat: admin customer request api

---
+++ src/main/java/com/alc/diary/presentation/api/admin/AdminCustomerRequestControllerV1.java
@@ -0,0 +1,30 @@
+package com.alc.diary.presentation.api.admin;
+
+import com.alc.diary.application.admin.customerrequest.AdminCustomerRequestDto;
+import com.alc.diary.application.admin.customerrequest.AdminCustomerRequestServiceV1;
+import com.alc.diary.presentation.dto.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/admin/v1/customer-requests")
+@RestController
+public class AdminCustomerRequestControllerV1 {
+
+    private final AdminCustomerRequestServiceV1 adminCustomerRequestService;
+
+    @GetMapping
+    public ApiResponse<Page<AdminCustomerRequestDto>> getAll(Pageable pageable) {
+        return ApiResponse.getSuccess(adminCustomerRequestService.getAll(pageable));
+    }
+
+    @GetMapping("/{customerRequestId}")
+    public ApiResponse<AdminCustomerRequestDto> getById(@PathVariable long customerRequestId) {
+        return ApiResponse.getSuccess(adminCustomerRequestService.getById(customerRequestId));
+    }
+}
